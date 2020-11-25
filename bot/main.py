ff711b8a33d8d1b7f119302ee1756784fd8e79d9
jjivanc <62190299+jjivanc@users.noreply.github.com>
2020-11-25 10:46:36 -0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -7,7 +7,7 @@ token = os.getenv("DISCORD_BOT_TOKEN")
 
 @client.event
 async def on_ready() :
-    await client.change_presence(status = discord.Status.idle, activity = discord.Game("Ativo  .help"))
+    await client.change_presence(status = discord.Status.idle, activity = discord.Game("A Vida Fora |.help para ajuda| "))
     print("I am online")
 
 @client.command()
